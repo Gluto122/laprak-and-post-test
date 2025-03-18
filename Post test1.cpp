9139f8607975f1ff30829dcd18f808fcfdb3d305
Gluto122 <mehdifiqy@gmail.com>
2025-03-18 17:59:38 +0800

    Update Post test1.cpp

--- Post test1.cpp
+++ Post test1.cpp
@@ -3,22 +3,24 @@
 using namespace std;
 
 
-
+//mendklarasikan fungsi utama 
 
 int main(){
+//mendeklarasikan tipe data
     string nama,nama_bensin;
     int harga,pilih;
     int liter;
     double total;
 	
-	
+	//menampilkan output dan menginput data 
 	cout<<"========== Pertamina ==========="<<endl;
 	cout<<"1.\t pertalite : rp.7000"<<endl;
 	cout<<"2.\t pertamax : rp.9000"<<endl;
 	cout<<"================================"<<endl;
 	cout<<"pilih jenis bensin: "; cin>>pilih;
 	cout<<endl;
-	
+
+	//mendklarasikan permisalan dan menginput ktikan ke dalam data
 	if(pilih == 1){
 		harga = 7000;
 		nama_bensin = "pertalite";
@@ -46,7 +48,7 @@ int main(){
 	
 	
 	
-	
+	//menampilkan isi dalam data
 	cout<<"=========================="<<endl;
 	cout<<"nama \t\t: "<<nama<<endl;
 	cout<<"jenis bensin\t: "<<nama_bensin<<endl;
